5b663a0e76293df8fa38d8857a1d33adb9123f5d
Ina Elise <113548269+Inaelise@users.noreply.github.com>
2025-04-21 13:51:38 +0200

    Adds routing to existing pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
-import { Header } from "./components/Header";
+import { Routes, Route } from "react-router-dom";
+import Layout from "./components/Layout";
+import Home from "./components/Home";
+import SpecificVenue from "./components/SpecificVenue";
+import Profile from "./components/Profile";
+import PageNotFound from "./components/PageNotFound";
 
 function App() {
   return (
-    <>
-      <Header />
-    </>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path="venue/:id" element={<SpecificVenue />} />
+        <Route path="profile" element={<Profile />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Route>
+    </Routes>
   );
 }
 
