a798445b4c84d0276c477ed7ed3b6df9ba72a471
Ina Elise <113548269+Inaelise@users.noreply.github.com>
2025-04-21 12:44:24 +0200

    Adds home component

---
+++ src/components/Home.jsx
@@ -0,0 +1,18 @@
+export default function Home() {
+  return (
+    <>
+      <meta
+        name="description"
+        content="Holidaze - find your place in the world. Book your stay now!"
+      />
+      <title>Holidaze</title>
+      <main>
+        <div>
+          <h1>Find your place in the world</h1>
+          {/* Add searchbar here */}
+        </div>
+        <div>{/* Add list of cards with pagination here */}</div>
+      </main>
+    </>
+  );
+}
