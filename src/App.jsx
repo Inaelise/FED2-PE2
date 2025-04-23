155696cf5ccdc6d1155bc1df14c419edcfc301f3
Ina Elise <113548269+Inaelise@users.noreply.github.com>
2025-04-23 10:42:11 +0200

    Fetches API venues and stores the data in state

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,31 @@ import SpecificVenue from "./components/SpecificVenue";
 import Profile from "./components/Profile";
 import PageNotFound from "./components/PageNotFound";
 import CreateVenue from "./components/CreateVenue";
+import { useEffect, useState } from "react";
+
+const API_URL = "https://v2.api.noroff.dev/holidaze/venues";
 
 function App() {
+  const [venues, setVenues] = useState([]);
+
+  useEffect(() => {
+    async function fetchVenues() {
+      try {
+        const response = await fetch(API_URL);
+        const json = await response.json();
+        setVenues(json.data);
+      } catch (error) {
+        console.error("Error fetching venues:", error);
+      }
+    }
+
+    fetchVenues();
+  }, []);
+
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
-        <Route index element={<Home />} />
+        <Route index element={<Home venues={venues} />} />
         <Route path="venue/:id" element={<SpecificVenue />} />
         <Route path="profile" element={<Profile />} />
         <Route path="create-venue" element={<CreateVenue />} />
