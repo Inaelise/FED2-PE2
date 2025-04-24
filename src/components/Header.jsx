8addede5e58c4e35dc7e2c681bcc37bc1871d300
Ina Elise <113548269+Inaelise@users.noreply.github.com>
2025-04-24 15:00:50 +0200

    Adds link to logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,18 @@
 import { LogOut } from "lucide-react";
 import { Navbar } from "./Navbar";
+import { NavLink } from "react-router-dom";
 
 export default function Header() {
   return (
     <header>
       <div>
-        <img
-          src="/images/holidaze-logo.png"
-          alt="Holidaze logo"
-          title="Go to home"
-        />
+        <NavLink to="/" title="Go to home">
+          <img
+            src="/images/holidaze-logo.png"
+            alt="Holidaze logo"
+            title="Go to home"
+          />
+        </NavLink>
         {/*Replace with logged in user*/}
         <p>Hello, username!</p>
         <button title="Click to log out">
