1cdb12767d75a88fa96c17e3678a085991a541b6
Ina Elise <113548269+Inaelise@users.noreply.github.com>
2025-04-26 12:51:17 +0200

    Adds login modal component with close button

---
+++ src/components/LoginModal.jsx
@@ -0,0 +1,33 @@
+import { X } from "lucide-react";
+import { Link } from "react-router-dom";
+
+export default function LoginModal({ onClose }) {
+  return (
+    <div>
+      <button onClick={onClose}>
+        <X />
+      </button>
+      <h1>Login</h1>
+      <div>
+        <p>Don't have an account?</p>
+        <Link to="/register">
+          <p>Register here</p>
+        </Link>
+      </div>
+      <form>
+        <label htmlFor="email">Email</label>
+        <input type="email" id="email" name="email" required />
+        <label htmlFor="password">Password</label>
+        <input type="password" id="password" name="password" required />
+        <div>
+          <div>
+            <input type="checkbox" id="remember" name="remember" />
+            <label htmlFor="remember">Remember me</label>
+          </div>
+          <a href="#">Forgot your password?</a>
+        </div>
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+}
