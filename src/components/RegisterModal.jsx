3aca094109cc07f39e3c19f5ab9071395a7c6c96
Ina Elise <113548269+Inaelise@users.noreply.github.com>
2025-04-28 14:40:41 +0200

    Adds react-hook-form and yup validation to register form and handles submit to api

--- src/components/RegisterModal.jsx
+++ src/components/RegisterModal.jsx
@@ -1,7 +1,56 @@
 import { X } from "lucide-react";
-import { Link } from "react-router-dom";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
+import { headers } from "../api/headers";
+
+const schema = yup.object({
+  venueManager: yup.boolean().optional(),
+  name: yup
+    .string()
+    .min(3, "Your name should be at least 3 characters.")
+    .max(20, "Your name can't be longer than 20 characters")
+    .required("Please enter a username."),
+  email: yup
+    .string()
+    .email("Invalid email.")
+    .required("Please enter an email."),
+  password: yup
+    .string()
+    .min(8, "Your password should be at least 8 characters.")
+    .required("Please enter a password."),
+});
 
 export default function RegisterModal({ onClose, switchModal }) {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+  } = useForm({ resolver: yupResolver(schema) });
+
+  async function registerUser({ venueManager, name, email, password }) {
+    const url = "https://v2.api.noroff.dev/auth/register";
+    const options = {
+      method: "POST",
+      headers: headers("application/json"),
+      body: JSON.stringify({ venueManager, name, email, password }),
+    };
+
+    try {
+      const response = await fetch(url, options);
+      if (!response.ok) {
+        throw new Error("Failed to register user.");
+      }
+      const json = await response.json();
+      console.log("Registration successful:", json);
+      reset();
+      onClose();
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  }
+
   return (
     <div>
       <button onClick={onClose}>
@@ -12,18 +61,33 @@ export default function RegisterModal({ onClose, switchModal }) {
         <p>Already have an account?</p>
         <p onClick={() => switchModal("login")}>Login here</p>
       </div>
-      <form>
+      <form onSubmit={handleSubmit(registerUser)}>
         <div>
-          <input type="checkbox" id="manager" name="manager" />
+          <input
+            type="checkbox"
+            id="manager"
+            name="manager"
+            {...register("venueManager")}
+          />
           <label htmlFor="manager">Venue manager</label>
         </div>
         <label htmlFor="name">name</label>
-        <input type="name" id="name" name="name" required />
+        <input type="name" id="name" name="name" {...register("name")} />
+        <p>{errors.name?.message}</p>
         <label htmlFor="email">Email</label>
-        <input type="email" id="email" name="email" required />
+        <input type="email" id="email" name="email" {...register("email")} />
+        <p>{errors.email?.message}</p>
         <label htmlFor="password">Password</label>
-        <input type="password" id="password" name="password" required />
-        <button type="submit">Register</button>
+        <input
+          type="password"
+          id="password"
+          name="password"
+          {...register("password")}
+        />
+        <p>{errors.password?.message}</p>
+        <button type="submit" title="Register account">
+          Register
+        </button>
       </form>
     </div>
   );
