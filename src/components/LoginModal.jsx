2fb1e9eb47b8ba6bb0a5df96008eeb8df7146e03
Ina Elise <113548269+Inaelise@users.noreply.github.com>
2025-04-28 14:40:32 +0200

    Adds react-hook-form and yup validation to login form and handles submit to api

--- src/components/LoginModal.jsx
+++ src/components/LoginModal.jsx
@@ -1,7 +1,53 @@
 import { X } from "lucide-react";
-import { Link } from "react-router-dom";
+import { save } from "../storage/save";
+import { headers } from "../api/headers";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
+import { useForm } from "react-hook-form";
+
+const schema = yup.object({
+  email: yup
+    .string()
+    .email("Invalid email.")
+    .required("Please enter your email."),
+  password: yup
+    .string()
+    .min(8, "Your password should be at least 8 characters.")
+    .required("Please enter your password."),
+});
 
 export default function LoginModal({ onClose, switchModal }) {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+  } = useForm({ resolver: yupResolver(schema) });
+
+  async function loginUser({ email, password }) {
+    const url = "https://v2.api.noroff.dev/auth/login";
+    const options = {
+      method: "POST",
+      headers: headers("application/json"),
+      body: JSON.stringify({ email, password }),
+    };
+
+    try {
+      const response = await fetch(url, options);
+      if (!response.ok) {
+        throw new Error("Failed to login user.");
+      }
+      const json = await response.json();
+      save("token", json.data.accessToken);
+      save("user", json.data.name);
+      console.log("Login successful:", json);
+      reset();
+      onClose();
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  }
+
   return (
     <div>
       <button onClick={onClose}>
@@ -12,11 +58,18 @@ export default function LoginModal({ onClose, switchModal }) {
         <p>Don't have an account?</p>
         <p onClick={() => switchModal("register")}>Register here</p>
       </div>
-      <form>
+      <form onSubmit={handleSubmit(loginUser)}>
         <label htmlFor="email">Email</label>
-        <input type="email" id="email" name="email" required />
+        <input type="email" id="email" name="email" {...register("email")} />
+        <p>{errors.email?.message}</p>
         <label htmlFor="password">Password</label>
-        <input type="password" id="password" name="password" required />
+        <input
+          type="password"
+          id="password"
+          name="password"
+          {...register("password")}
+        />
+        <p>{errors.password?.message}</p>
         <div>
           <div>
             <input type="checkbox" id="remember" name="remember" />
@@ -24,7 +77,9 @@ export default function LoginModal({ onClose, switchModal }) {
           </div>
           <a href="#">Forgot your password?</a>
         </div>
-        <button type="submit">Login</button>
+        <button type="submit" title="Login">
+          Login
+        </button>
       </form>
     </div>
   );
