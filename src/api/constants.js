94a55f3e87f4b3d74d632e950706107c2a1ac84f
Ina Elise <113548269+Inaelise@users.noreply.github.com>
2025-04-29 12:30:53 +0200

    Adds api key and endpoints

---
+++ src/api/constants.js
@@ -0,0 +1,11 @@
+export const API_KEY = "c498996c-21dc-480e-979a-7cd703127955";
+
+export const API_BASE = "https://v2.api.noroff.dev";
+
+export const API_AUTH_LOGIN = `${API_BASE}/auth/login`;
+
+export const API_AUTH_REGISTER = `${API_BASE}/auth/register`;
+
+export const API_HOLIDAZE_VENUES = `${API_BASE}/holidaze/venues`;
+
+export const API_HOLIDAZE_PROFILES = `${API_BASE}/holidaze/profiles`;
