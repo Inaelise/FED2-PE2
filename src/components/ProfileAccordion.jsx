ad55796278ebde30be353c765602edda4f2572b0
Ina Elise <113548269+Inaelise@users.noreply.github.com>
2025-05-04 15:31:33 +0200

    Adds conditional check for api info and links

--- src/components/ProfileAccordion.jsx
+++ src/components/ProfileAccordion.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { ChevronDown, ChevronUp } from "lucide-react";
+import { Link } from "react-router";
 
 export default function ProfileAccordion({ user }) {
   const [open, setOpen] = useState(null);
@@ -15,48 +16,71 @@ export default function ProfileAccordion({ user }) {
         {open === "bookings" ? <ChevronUp /> : <ChevronDown />}
       </div>
       {open === "bookings" && (
-        <ul>
-          {user.bookings.map((booking) => (
-            <li key={booking.id}>
-              <div>
-                <p>Venue name:</p>
-                <p>{booking.venue.name}</p>
-              </div>
-              <div>
-                <p>Dates:</p>
-                <p>
-                  {new Date(booking.dateFrom).toLocaleDateString("no-NO")} -{" "}
-                  {new Date(booking.dateTo).toLocaleDateString("no-NO")}
-                </p>
-              </div>
-              <div>
-                <p>Guests:</p>
-                <p>{booking.guests}</p>
-              </div>
-            </li>
-          ))}
-        </ul>
+        <div>
+          {user.bookings?.length > 0 ? (
+            <ul>
+              {user.bookings.map((booking) => (
+                <li key={booking.id}>
+                  <div>
+                    <p>Venue name:</p>
+                    <Link to={`/venue/${booking.venue.id}`} title="View venue">
+                      <p>{booking.venue.name}</p>
+                    </Link>
+                  </div>
+                  <div>
+                    <p>Dates:</p>
+                    <p>
+                      {new Date(booking.dateFrom).toLocaleDateString("no-NO")} -{" "}
+                      {new Date(booking.dateTo).toLocaleDateString("no-NO")}
+                    </p>
+                  </div>
+                  <div>
+                    <p>Guests:</p>
+                    <p>{booking.guests}</p>
+                  </div>
+                </li>
+              ))}
+            </ul>
+          ) : (
+            <div>
+              <p>No bookings found.</p>
+              <Link to={"/"} title="Go to home page">
+                Book a stay here
+              </Link>
+            </div>
+          )}
+        </div>
       )}
       <div onClick={() => toggleSection("venues")}>
-        <h2>Your venues({user.venues?.length})</h2>
+        <h2>Your venues ({user.venues?.length})</h2>
         {open === "venues" ? <ChevronUp /> : <ChevronDown />}
       </div>
       {open === "venues" && (
-        <ul>
-          {user.venues?.map((venue) => (
-            <li key={venue.id}>
-              <img
-                src={venue.media[0]?.url}
-                alt={venue.media[0]?.alt || "Venue thumbnail"}
-              />
-              <p>{venue.name}</p>
-            </li>
-          ))}
-        </ul>
+        <div>
+          {user.venues?.length > 0 ? (
+            <ul>
+              {user.venues?.map((venue) => (
+                <li key={venue.id}>
+                  <Link to={`/venue/${venue.id}`} title="View venue">
+                    <img
+                      src={venue.media[0]?.url}
+                      alt={venue.media[0]?.alt || "Venue thumbnail"}
+                    />
+                    <p>{venue.name}</p>
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          ) : (
+            <div>
+              <p>No venues found.</p>
+              <Link to={"/create-venue"} title="Go to create venue page">
+                Create venue here
+              </Link>
+            </div>
+          )}
+        </div>
       )}
-      <div>
-        <h2>Your venue bookings ({/* Add customer bookings */})</h2>
-      </div>
     </div>
   );
 }
