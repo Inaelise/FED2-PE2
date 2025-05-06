46572a6f4098e62579bc8f24775a5c599c31b71e
Ina Elise <113548269+Inaelise@users.noreply.github.com>
2025-05-06 14:26:00 +0200

    Adds jsdocs

--- src/api/venue/read.js
+++ src/api/venue/read.js
@@ -1,5 +1,17 @@
 import { API_HOLIDAZE_VENUES } from "../constants";
 
+/**
+ * Fetches a paginated list of venues from the API.
+ * If search query is provided, it fetched search results instead.
+ *
+ * @param {Object} params - Parameters for the API request.
+ * @param {number} page - The page number to fetch (default is 1).
+ * @param {string} query - The search query to filter venues (default is empty).
+ * @returns {Promise<Object>} - The response data containing the list of venues.
+ * @throws {Error} - Throws an error if the API request fails.
+ * @example
+ * const = { data, meta } = await getVenues({ page: 2, query: "spain" });
+ */
 export async function getVenues({ page = 1, query = "" }) {
   const baseUrl = query
     ? `${API_HOLIDAZE_VENUES}/search?q=${encodeURIComponent(query)}`
