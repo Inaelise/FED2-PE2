6691e67e56ed843d1cfef7e8d6cc308fff709da9
Ina Elise <113548269+Inaelise@users.noreply.github.com>
2025-05-06 14:19:50 +0200

    Passes isLoading and isError and adds conditional rendering

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,8 @@
 import SearchBar from "./SearchBar";
 import VenueCard from "./VenueCard";
 import Pagination from "./Pagination";
+import { CircleAlert } from "lucide-react";
+import LoadingSpinner from "./LoadingSpinner";
 
 export default function Home({
   venues,
@@ -8,6 +10,9 @@ export default function Home({
   currentPage,
   setCurrentPage,
   meta,
+  searchQuery,
+  isLoading,
+  isError,
 }) {
   return (
     <>
@@ -19,34 +24,45 @@ export default function Home({
       <main>
         <div>
           <h1>Find your place in the world</h1>
-          <SearchBar onSearch={onSearch} query={""} />
+          <SearchBar onSearch={onSearch} query={searchQuery} />
         </div>
-        <div>
-          {venues.length > 0 ? (
-            <ul>
-              {venues.map((venue) => (
-                <li key={venue.id}>
-                  <VenueCard
-                    id={venue.id}
-                    img={venue.media?.[0]}
-                    name={venue.name}
-                    location={venue.location}
-                    price={venue.price}
-                    maxGuests={venue.maxGuests}
-                    rating={venue.rating}
-                  />
-                </li>
-              ))}
-            </ul>
-          ) : (
-            <p>No venues found.</p>
-          )}
-        </div>
-        <Pagination
-          meta={meta}
-          currentPage={currentPage}
-          setCurrentPage={setCurrentPage}
-        />
+        {isLoading ? (
+          <LoadingSpinner />
+        ) : isError ? (
+          <div>
+            <CircleAlert />
+            <p>Oops, something went wrong. Could not load data.</p>
+          </div>
+        ) : (
+          <div>
+            <div>
+              {venues.length > 0 ? (
+                <ul>
+                  {venues.map((venue) => (
+                    <li key={venue.id}>
+                      <VenueCard
+                        id={venue.id}
+                        img={venue.media?.[0]}
+                        name={venue.name}
+                        location={venue.location}
+                        price={venue.price}
+                        maxGuests={venue.maxGuests}
+                        rating={venue.rating}
+                      />
+                    </li>
+                  ))}
+                </ul>
+              ) : (
+                <p>No venues found.</p>
+              )}
+            </div>
+            <Pagination
+              meta={meta}
+              currentPage={currentPage}
+              setCurrentPage={setCurrentPage}
+            />
+          </div>
+        )}
       </main>
     </>
   );
