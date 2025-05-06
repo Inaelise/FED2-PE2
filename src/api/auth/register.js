2e3584028417acf478b207a8259bc1834a856d21
Ina Elise <113548269+Inaelise@users.noreply.github.com>
2025-05-06 13:10:54 +0200

    Adds api function to register with jsdocs

---
+++ src/api/auth/register.js
@@ -0,0 +1,31 @@
+import { headers } from "../headers";
+import { API_AUTH_REGISTER } from "../constants";
+
+/**
+ * Registers a new user by sending their details to the API.
+ *
+ * @param {Object} params - The registration details.
+ * @param {boolean} venueManager - Venue manager status.
+ * @param {string} name - The user's name.
+ * @param {string} email - The user's email address.
+ * @param {string} password - The user's password.
+ * @returns {Promise<Object>} The registered user data.
+ * @throws {Error} If the registration fails, an error message is thrown.
+ */
+export async function registerUser({ venueManager, name, email, password }) {
+  const options = {
+    method: "POST",
+    headers: headers("application/json"),
+    body: JSON.stringify({ venueManager, name, email, password }),
+  };
+
+  const response = await fetch(API_AUTH_REGISTER, options);
+  const json = await response.json();
+
+  if (!response.ok) {
+    const errorMessage = json.errors.map((error) => error.message).join("\r\n");
+    throw new Error(errorMessage);
+  }
+
+  return json.data;
+}
