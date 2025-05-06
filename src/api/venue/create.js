91f77bdb779a813d512aa31eeda2ca4f573282df
Ina Elise <113548269+Inaelise@users.noreply.github.com>
2025-05-06 15:02:19 +0200

    Adds api function for creating venue and jsdocs

---
+++ src/api/venue/create.js
@@ -0,0 +1,24 @@
+import { API_HOLIDAZE_VENUES } from "../constants";
+import { headers } from "../headers";
+
+/**
+ * Sends a request to the API to create a new venue.
+ *
+ * @param {Object} data - The venue data to be sent to the API.
+ * @throws {Error} - Throws an error if the API request fails.
+ */
+export async function createVenue(data) {
+  const options = {
+    method: "POST",
+    headers: headers("application/json"),
+    body: JSON.stringify(data),
+  };
+
+  const response = await fetch(API_HOLIDAZE_VENUES, options);
+  const json = await response.json();
+
+  if (!response.ok) {
+    const errorMessage = json.errors.map((error) => error.message).join("\r\n");
+    throw new Error(errorMessage);
+  }
+}
