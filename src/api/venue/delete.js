8d1b6953ebe38a57da0b7bfd081df3d93372a849
Ina Elise <113548269+Inaelise@users.noreply.github.com>
2025-05-07 12:23:17 +0200

    Adds jsdocs

--- src/api/venue/delete.js
+++ src/api/venue/delete.js
@@ -1,6 +1,12 @@
 import { API_HOLIDAZE_VENUES } from "../constants";
 import { headers } from "../headers";
 
+/**
+ * Sends a request to the API to delete a venue.
+ *
+ * @param {string} id - The ID of the venue to be deleted.
+ * @throws {Error} - Throws an error if the API request fails.
+ */
 export async function deleteVenue(id) {
   const options = {
     method: "DELETE",
