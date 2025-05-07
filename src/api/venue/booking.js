cbe0d4195df8ddf1bee6f98ef9b51401ca29ca82
Ina Elise <113548269+Inaelise@users.noreply.github.com>
2025-05-07 12:33:21 +0200

    Adds jsdocs

--- src/api/venue/booking.js
+++ src/api/venue/booking.js
@@ -1,6 +1,13 @@
 import { headers } from "../headers";
 import { API_HOLIDAZE_BOOKINGS } from "../api/constants";
 
+/**
+ * Sends a request to the API to create a booking.
+ *
+ * @param {Object} data - The booking data to be sent to the API.
+ * @returns {Promise<Object>} - The created booking data.
+ * @throws {Error} - Throws an error if the API request fails.
+ */
 export async function createBooking(data) {
   const options = {
     method: "POST",
