e6bc9f4b0f4a27304cab7bb03f2f323e2e3d826a
Ina Elise <113548269+Inaelise@users.noreply.github.com>
2025-05-07 14:47:16 +0200

    Adds conditional rendering for ul and login/register buttons

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,6 +9,7 @@ import { NavLink } from "react-router";
 import { useState } from "react";
 import LoginModal from "./LoginModal";
 import RegisterModal from "./RegisterModal";
+import { load } from "../storage/load";
 
 /**
  * The navbar component that contains links to different pages and modals for login and registration.
@@ -18,6 +19,7 @@ import RegisterModal from "./RegisterModal";
  */
 export function Navbar() {
   const [openModal, setOpenModal] = useState(null);
+  const activeUser = load("user");
 
   const switchModal = (modal) => {
     setOpenModal(modal);
@@ -25,31 +27,39 @@ export function Navbar() {
 
   return (
     <nav>
-      <ul>
-        <li title="Go to home">
-          <NavLink to="/">
-            <Hotel size={16} strokeWidth={1.5} /> Venues
-          </NavLink>
-        </li>
-        <li title="Go to profile">
-          <NavLink to="profile">
-            <CircleUser size={16} strokeWidth={1.5} /> Profile
-          </NavLink>
-        </li>
-        <li title="Go to create venue">
-          <NavLink to="create-venue">
-            <CirclePlus size={16} strokeWidth={1.5} /> Add venue
-          </NavLink>
-        </li>
-      </ul>
-      <button onClick={() => setOpenModal("login")}>
-        <LogIn size={16} strokeWidth={1.5} />
-        Login
-      </button>
-      <button onClick={() => setOpenModal("register")}>
-        <UserRoundPlus size={16} strokeWidth={1.5} />
-        Register
-      </button>
+      {activeUser && (
+        <ul>
+          <li title="Go to home">
+            <NavLink to="/">
+              <Hotel size={16} strokeWidth={1.5} /> Venues
+            </NavLink>
+          </li>
+          <li title="Go to profile">
+            <NavLink to="profile">
+              <CircleUser size={16} strokeWidth={1.5} /> Profile
+            </NavLink>
+          </li>
+          <li title="Go to create venue">
+            <NavLink to="create-venue">
+              <CirclePlus size={16} strokeWidth={1.5} /> Add venue
+            </NavLink>
+          </li>
+        </ul>
+      )}
+
+      {!activeUser && (
+        <div>
+          <button onClick={() => setOpenModal("login")}>
+            <LogIn size={16} strokeWidth={1.5} />
+            Login
+          </button>
+          <button onClick={() => setOpenModal("register")}>
+            <UserRoundPlus size={16} strokeWidth={1.5} />
+            Register
+          </button>
+        </div>
+      )}
+
       {openModal === "login" && (
         <LoginModal
           onClose={() => setOpenModal(null)}
