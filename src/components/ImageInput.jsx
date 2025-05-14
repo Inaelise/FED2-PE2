a2675ec71ecfe5bba7f0f279d891e9c9fcdf5870
Ina Elise <113548269+Inaelise@users.noreply.github.com>
2025-05-14 16:46:22 +0200

    Adds styling classes to elements

--- src/components/ImageInput.jsx
+++ src/components/ImageInput.jsx
@@ -54,28 +54,48 @@ export default function ImageInput({ onChange, value = [] }) {
   };
 
   return (
-    <div>
-      <div>
-        <label htmlFor="images">Images</label>
-        <p>(URL)</p>
+    <div className="flex flex-col gap-6 py-4">
+      <div className="flex flex-col gap-1">
+        <div className="flex gap-2">
+          <label htmlFor="images" className="label-primary">
+            Images
+          </label>
+          <p className="opacity-40">(URL)</p>
+        </div>
+        <div className="flex flex-col gap-4">
+          <input
+            id="images"
+            placeholder="https://exampleurl.com"
+            value={imageUrl}
+            onChange={(e) => {
+              setImageUrl(e.target.value);
+              if (error) setError(false);
+            }}
+            className="input-primary"
+          />
+          <button
+            onClick={handleAddImage}
+            className="bg-green text-white font-poppins font-semibold w-20 py-2 mx-auto rounded-2xl hover:bg-[#2c3a2d] animate cursor-pointer"
+          >
+            Add
+          </button>
+        </div>
       </div>
-      <input
-        id="images"
-        placeholder="https://exampleurl.com"
-        value={imageUrl}
-        onChange={(e) => {
-          setImageUrl(e.target.value);
-          if (error) setError(false);
-        }}
-      />
-      <button onClick={handleAddImage}>Add</button>
       {error && <p>Image already added. Try a different url.</p>}
-      <div>
+
+      <div className="flex justify-center gap-6 flex-wrap">
         {value.map((img) => (
-          <div key={img.url}>
-            <img src={img.url} alt={img.alt} />
-            <button onClick={() => handleRemoveImage(img.url)}>
-              <X />
+          <div key={img.url} className="relative">
+            <img
+              src={img.url}
+              alt={img.alt}
+              className="w-28 h-28 object-cover rounded-2xl drop-shadow-base"
+            />
+            <button
+              onClick={() => handleRemoveImage(img.url)}
+              className="absolute top-1 right-1 bg-white rounded-full p-1 drop-shadow-s"
+            >
+              <X size={18} />
             </button>
           </div>
         ))}
