d77ff17b18ed16a7a42471a594660c2260482d2b
Ina Elise <113548269+Inaelise@users.noreply.github.com>
2025-05-15 17:10:50 +0200

    Adds div with white overlay and styling classes to elements

--- src/components/PageNotFound.jsx
+++ src/components/PageNotFound.jsx
@@ -13,13 +13,18 @@ export default function PageNotFound() {
         content="The page you're looking for does not exist."
       />
       <title>Page Not Found</title>
-      <main>
-        <div>
-          <h1>Page not found</h1>
-          <p>Oops, the page you are looking for does not exist.</p>
-          <NavLink to="/">
-            <button>Go to home</button>
-          </NavLink>
+      <main className="font-inter">
+        <div className="relative bg-[url('/images/pastel-street-compressed.jpg')] bg-cover bg-right h-[400px] mt-30 mx-auto max-w-[700px]">
+          <div className="absolute inset-0 bg-white opacity-50" />
+          <div className="absolute inset-0 z-5 flex flex-col items-center justify-center text-center text-green gap-6">
+            <h1 className="font-bold text-l">Page not found</h1>
+            <p>Oops, the page you are looking for does not exist.</p>
+            <NavLink to="/">
+              <button className="bg-orange font-semibold text-white drop-shadow-base py-1.5 px-18 rounded-full cursor-pointer mt-6 hover animate">
+                Go to home
+              </button>
+            </NavLink>
+          </div>
         </div>
       </main>
     </>
