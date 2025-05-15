cf377d016c4e34f7be7dd8856620c7b1613df868
Ina Elise <113548269+Inaelise@users.noreply.github.com>
2025-05-15 09:20:19 +0200

    Adds styling classes to elements

--- src/components/CreateVenueForm.jsx
+++ src/components/CreateVenueForm.jsx
@@ -94,95 +94,141 @@ export default function CreateVenueForm() {
         content="Create a venue by filling out the form."
       />
       <title>Create venue</title>
-      <main>
-        <h1>Create venue</h1>
-        <form onSubmit={handleSubmit(onCreate)}>
-          <div>
-            <label htmlFor="name">Name</label>
+      <main className="px-6 py-10">
+        <h1 className="text-green text-l font-semibold font-poppins pb-8 text-center">
+          Create venue
+        </h1>
+        <form
+          onSubmit={handleSubmit(onCreate)}
+          className="flex flex-col gap-10"
+        >
+          <div className="flex flex-col gap-1">
+            <label htmlFor="name" className="label-primary">
+              Name
+            </label>
             <input
-              className="input"
+              className="input-primary"
               id="name"
               placeholder="Enter a venue name"
               {...register("name")}
             />
             <p>{errors.name?.message}</p>
           </div>
-          <div>
-            <label htmlFor="description">Description</label>
+          <div className="flex flex-col gap-1">
+            <label htmlFor="description" className="label-primary">
+              Description
+            </label>
             <textarea
               id="description"
               placeholder="Enter a description"
+              className="input-primary"
               {...register("description")}
             />
             <p>{errors.description?.message}</p>
           </div>
-          <div>
-            <label htmlFor="price">Price</label>
-            <p>(Per night)</p>
-            <input id="price" {...register("price")} />
-            <p>{errors.price?.message}</p>
-          </div>
-          <div>
-            <label htmlFor="guests">Max guests</label>
-            <input id="guests" {...register("maxGuests")} />
-            <p>{errors.guests?.message}</p>
+          <div className="flex items-center justify-between">
+            <div className="flex flex-col gap-1">
+              <div className="flex gap-2">
+                <label htmlFor="price" className="label-primary">
+                  Price
+                </label>
+                <p className="text-sm opacity-40">(Per night)</p>
+              </div>
+              <input
+                id="price"
+                className="input-primary max-w-30"
+                {...register("price")}
+              />
+              <p>{errors.price?.message}</p>
+            </div>
+            <div className="flex flex-col gap-1">
+              <label htmlFor="guests" className="label-primary">
+                Max guests
+              </label>
+              <input
+                id="guests"
+                className="input-primary max-w-30"
+                {...register("maxGuests")}
+              />
+              <p>{errors.guests?.message}</p>
+            </div>
           </div>
           <fieldset>
-            <legend>Amenities</legend>
-            <div>
-              <label>
+            <legend className="label-primary">Amenities</legend>
+            <div className="grid grid-cols-2 grid-rows-2 gap-8 text-sm py-4">
+              <label className="flex gap-2">
                 <input type="checkbox" {...register("meta.wifi")} />
                 Free wifi
               </label>
-            </div>
-            <div>
-              <label>
+              <label className="flex gap-2">
                 <input type="checkbox" {...register("meta.breakfast")} />
                 Breakfast
               </label>
-            </div>
-            <div>
-              <label>
+              <label className="flex gap-2">
                 <input type="checkbox" {...register("meta.parking")} />
                 Parking
               </label>
-            </div>
-            <div>
-              <label>
+              <label className="flex gap-2">
                 <input type="checkbox" {...register("meta.pets")} />
                 Pets
               </label>
             </div>
           </fieldset>
           <div>
-            <p>Rating</p>
+            <p className="label-primary pb-2">Rating</p>
             <Rating onChange={(value) => setValue("rating", value)} />
           </div>
-          <div>
-            <label htmlFor="country">Country</label>
-            <input id="country" {...register("location.country")} />
+          <div className="flex flex-col gap-1">
+            <label htmlFor="country" className="label-primary">
+              Country
+            </label>
+            <input
+              id="country"
+              className="input-primary"
+              {...register("location.country")}
+            />
             <p>{errors.country?.message}</p>
           </div>
-          <div>
-            <label htmlFor="city">City</label>
-            <input id="city" {...register("location.city")} />
+          <div className="flex flex-col gap-1">
+            <label htmlFor="city" className="label-primary">
+              City
+            </label>
+            <input
+              id="city"
+              className="input-primary"
+              {...register("location.city")}
+            />
             <p>{errors.city?.message}</p>
           </div>
-          <div>
-            <label htmlFor="address">Address</label>
-            <input id="address" {...register("location.address")} />
+          <div className="flex flex-col gap-1">
+            <label htmlFor="address" className="label-primary">
+              Address
+            </label>
+            <input
+              id="address"
+              className="input-primary"
+              {...register("location.address")}
+            />
             <p>{errors.address?.message}</p>
           </div>
-          <div>
-            <label htmlFor="zip">Zip code</label>
-            <input id="zip" {...register("location.zip")} />
+          <div className="flex flex-col gap-1">
+            <label htmlFor="zip" className="label-primary">
+              Zip code
+            </label>
+            <input
+              id="zip"
+              className="input-primary"
+              {...register("location.zip")}
+            />
             <p>{errors.zip?.message}</p>
           </div>
           <ImageInput
             value={watch("media")}
             onChange={(images) => setValue("media", images)}
           />
-          <button type="submit">Create venue</button>
+          <button type="submit" className="btn-form bg-green hover animate">
+            Create venue
+          </button>
         </form>
       </main>
     </>
