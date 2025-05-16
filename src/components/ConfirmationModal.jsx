e1746dab41c7a05b25dba59cc44e67f40112bfe3
Ina Elise <113548269+Inaelise@users.noreply.github.com>
2025-05-16 15:27:25 +0200

    Adds styling classes to elements

--- src/components/ConfirmationModal.jsx
+++ src/components/ConfirmationModal.jsx
@@ -17,17 +17,29 @@ export default function ConfirmationModal({
   onConfirm,
 }) {
   return (
-    <div>
-      <div>
-        <button onClick={onCancel}>
-          <X />
-        </button>
-      </div>
-      <h2>{title}</h2>
-      <p>{message}</p>
-      <div>
-        <button onClick={onConfirm}>Yes</button>
-        <button onClick={onCancel}>No</button>
+    <div className="overlay items-center">
+      <div className="relative p-4 sm:px-8 bg-white font-inter drop-shadow-base max-w-[500px] rounded-xl text-center text-black">
+        <div>
+          <button onClick={onCancel} className="btn-close">
+            <X />
+          </button>
+        </div>
+        <h2 className="text-m font-bold py-6">{title}</h2>
+        <p>{message}</p>
+        <div className="my-6 flex justify-center gap-16">
+          <button
+            onClick={onConfirm}
+            className="btn-confirm bg-green-500 font-bold text-white hover animate"
+          >
+            Yes
+          </button>
+          <button
+            onClick={onCancel}
+            className="btn-confirm border-2 border-[#00000050] hover animate"
+          >
+            No
+          </button>
+        </div>
       </div>
     </div>
   );
